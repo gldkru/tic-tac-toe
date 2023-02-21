11398ecc0acf10145e8523194c8638899793b49b
Kirill Gladkikh <onest.kill@yandex.ru>
2023-02-21 06:44:52 +0000

    feat: generate grid

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const app = document.getElementById("app");
+
+const grid = document.createElement("div");
+grid.classList.add("grid");
+
+for (let i = 0; i < 9; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+
+  cell.textContent = i;
+  cell.dataset.id = i;
+
+  grid.appendChild(cell);
+}
+
+app.appendChild(grid);
